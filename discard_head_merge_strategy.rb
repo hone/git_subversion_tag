48da8d03275a756269fb8796fbb521286111772e
Terence Lee <hone02@gmail.com>
2009-07-09 13:34:28 -0500

    update README and add licensing

--- discard_head_merge_strategy.rb
+++ discard_head_merge_strategy.rb
@@ -1,3 +1,21 @@
+=begin
+  DiscardHeadMergeStrategy
+  discard_head_merge_strategy.rb
+  Copyright (C) 2009 Terence Lee <hone02@gmail.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=end
 # when merging in git and get a conflicted file, this will discard all head changes and keep the changes from the incoming merge file
 class DiscardHeadMergeStrategy
   CONFLICT_HEAD_CHARS  = '<<<<<<<'
