e4d0f5ca51be076f5ad6e09eb46d1609d19b7533
Terence Lee <hone02@gmail.com>
2009-07-13 16:35:33 -0500

    use better require handling

--- subversion_tag.rb
+++ subversion_tag.rb
@@ -16,7 +16,8 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =end
-require 'discard_head_merge_strategy'
+require 'pathname'
+require "#{Pathname.new(__FILE__).dirname}/discard_head_merge_strategy"
 
 class SubversionTag
   class << self
